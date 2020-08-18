f47c4f66a19e7ace045c976a0f84ec32155f260c
Vladislav Beliaev <46963052+vladikbelyaew@users.noreply.github.com>
2020-08-18 14:21:16 +0300

    Create ReverseString.scala

---
+++ ReverseString.scala
@@ -0,0 +1,16 @@
+object Solution {
+  def reverseString(s: Array[Char]): Unit = {
+    val l: Int = s.length
+    val n: Int = l / 2
+    var buf: Char = 0
+    def swap(count: Int = 0): Unit = {
+      if (count < n) {
+        buf = s(count)
+        s(count) = s(l - 1 - count)
+        s(l - 1 - count) = buf
+        swap(count + 1)
+      }
+    }
+    swap()
+  }
+}
