01ded34cbf05e8077c603c136553e6f5b840428f
Vladislav Beliaev <46963052+vladikbelyaew@users.noreply.github.com>
2020-08-24 14:54:29 +0300

    Create IntegerToRoman.scala

---
+++ Medium/IntegerToRoman.scala
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/integer-to-roman/
+
+object Solution {
+  
+  val ints: List[Int] = List(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+  val romans: List[String] = List("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+  
+  def intToRoman(num: Int, count: Int = 0, res: String = ""): String = {
+    if (num == 0) res
+    else if (num < ints(count)) intToRoman(num, count + 1, res)
+    else intToRoman(num - ints(count), count, res + romans(count))
+  }
+  
+}
