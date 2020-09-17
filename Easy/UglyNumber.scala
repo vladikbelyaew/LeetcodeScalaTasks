8136b3dc5737bfbc08018bd0895da205c365c0f5
Vladislav Beliaev <46963052+vladikbelyaew@users.noreply.github.com>
2020-09-17 14:40:11 +0300

    Create UglyNumber.scala

---
+++ Easy/UglyNumber.scala
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/ugly-number/
+
+object Solution {
+  def isUgly(num: Int): Boolean = {
+    if (num == 1) true
+    else if (num <= 0) false
+    else if (num % 2 == 0) isUgly(num / 2)
+    else if (num % 3 == 0) isUgly(num / 3)
+    else if (num % 5 == 0) isUgly(num / 5)
+    else false
+  }
+}
